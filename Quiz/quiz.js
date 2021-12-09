a30aa859bba0c0c197784a691dc37399f1670f6b
Prejin Senan <93463606+prejin2310@users.noreply.github.com>
2021-12-09 13:17:27 +0530

    Pgm 10 - Final Commit

---
+++ Quiz/quiz.js
@@ -0,0 +1,17 @@
+function result(){
+	let score=0
+
+	 if(document.getElementById('q13').checked){
+	 	score+=1
+	 }
+	 if(document.getElementById('q22').checked){
+	 	score+=1
+	 }
+	 if(document.getElementById('q31').checked){
+	 	score+=1
+	 }
+
+
+
+alert("You Scored: "+score)
+}
